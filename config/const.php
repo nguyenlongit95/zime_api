b424c36704f85928894141b1da24e14f2e9d4cfe
longnct7090 <longnct7090@cowell.com.vn>
2022-04-04 10:44:56 +0700

    check router

---
+++ config/const.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'paginate' => 10,
+    'user' => [
+        'admin' => 0,
+        'user' => 1,
+    ],
+    'package' => [
+        'max_file_size' => [
+            16,
+            64,
+            128
+        ],
+    ],
+];
